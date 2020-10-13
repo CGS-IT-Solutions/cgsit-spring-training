0d8e70baf07c2c1bf33d4805acc8aa31ee4c02c5
ext_christian.schaef <christian.schaefer@cgs.at>
2020-10-13 13:13:21 +0200

    #1 initial commit

--- cgsit-spring-rest-endpoint/src/test/java/at/cgsit/training/restendpoint/rest/SimpleMongoDbRestControllerTemplateTest.java
+++ cgsit-spring-rest-endpoint/src/test/java/at/cgsit/training/restendpoint/rest/SimpleMongoDbRestControllerTemplateTest.java
@@ -25,7 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class SimpleMongoDbRestControllerTest {
+class SimpleMongoDbRestControllerTemplateTest {
 
     @LocalServerPort
     private int port;
